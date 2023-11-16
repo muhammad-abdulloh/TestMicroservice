6b0a9a8a7a64ae9968127bb08677d432abf9fcc9
muhammad-abdulloh <clevercoderr@gmail.com>
2023-11-16 22:16:26 +0500

    Add project files.

---
+++ Backet.API/Controllers/BacketsController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Backet.API.Controllers
+{
+    [Route("api/[controller]/[action]")]
+    [ApiController]
+    public class BacketsController : ControllerBase
+    {
+        [HttpGet]
+        public IActionResult GetAll()
+        {
+            return Ok("Bu Backet.API dan kelyapti");
+        }
+    }
+}
---
+++ Catalog.API/Controllers/CatalogsController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Catalog.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CatalogsController : ControllerBase
+    {
+        [HttpGet]
+        public IActionResult Get()
+        {
+            return Ok("Bu Catalog.API dan kelyapti");
+        }
+    }
+}
